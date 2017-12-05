455ca2d4f2c6b77e36130db72fc41a6b96ab536e
MikeYu <qianchenzhumeng@live.cn>
2017-12-05 11:02:10 +0800

    修改PLLDIV为3

--- smoke-alarm-platform/stm32cube-lib/stm32cube-prj/Src/stm32cube_hal_init.c
+++ smoke-alarm-platform/stm32cube-lib/stm32cube-prj/Src/stm32cube_hal_init.c
@@ -143,7 +143,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL3;
-  RCC_OscInitStruct.PLL.PLLDIV = RCC_PLL_DIV4;
+  RCC_OscInitStruct.PLL.PLLDIV = RCC_PLL_DIV3;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
